d25de051b4a9188c367cec8583d1286c8303768b
Robert K. Bell <robert.k.bell@gmail.com>
2016-08-28 18:38:21 +1000

    feat: load restaurants, reviews via xhr

--- public/js/main/app.js
+++ public/js/main/app.js
@@ -1,26 +1,56 @@
 import xs from 'xstream';
 import { run } from '@cycle/xstream-run';
-import { div, p, hr, makeDOMDriver } from '@cycle/dom';
+import { div, p, hr, pre, makeDOMDriver } from '@cycle/dom';
 
 import switchPath from 'switch-path';
-import {createHistory} from 'history';
+import { createHistory } from 'history';
+import { makeHTTPDriver } from '@cycle/http';
 import { makeHistoryDriver } from '@cycle/history';
 
 import view from './components/RestaurantList/view';
 
 function main(sources) {
+  const request$ = xs.of(
+    {
+      url: '/data/restaurants.json',
+      category: 'restaurant-list'
+    },
+    {
+      url: '/data/reviews.json',
+      category: 'all-reviews-list'
+    }
+  );
+
+  let restaurantsData$ = sources.HTTP
+    .select('restaurant-list')
+    .flatten()
+    .map(res => res.text)
+    /*.startWith(null)*/;
+
+  let allReviewsData$ = sources.HTTP
+    .select('all-reviews-list')
+    .flatten()
+    .map(res => res.text)
+    /*.startWith(null)*/;
+
   //noinspection UnnecessaryLocalVariableJS
   const sinks = {
-    DOM: sources.History
-      .startWith('???')
-      .map(x =>
+    DOM: xs.combine(
+      sources.History,
+      restaurantsData$,
+      allReviewsData$
+    )
+      // .startWith('???')
+      .map(([history, restaurants, reviews]) =>
         div([
           view.headerVdom(),
           hr(),
-          p('Hello ' + x),
-          view.locationProps(x)
+          view.locationProps(history),
+          pre(restaurants),
+          pre(reviews),
         ])
-      )
+      ),
+    HTTP: request$
   };
   return sinks;
 }
@@ -28,4 +58,5 @@ function main(sources) {
 run(main, {
   DOM: makeDOMDriver('#app-container'),
   History: makeHistoryDriver(createHistory()),
+  HTTP: makeHTTPDriver(),
 });
